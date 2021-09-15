c3d2375f9f68c8c7e6f0723bb9bfb0cc58a40773
Carrie <carr1eg@hotmail.com>
2021-09-15 15:46:04 -0700

    prepare seed file

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO users (id, name, email, password)
+VALUES (1, 'Sue Luna ', 'jasonvincent@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(2, 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+(3, 'makaylaweiss@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (id, owner_id, title, description, thumbnail_photo_url,
+cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+VALUES 
+(1, 1, 'Cozy Room in Burnaby', 'description', 'tinyurl.com', 'tinyurl.com', 190, 1, 2, 2),
+(2, 2, 'Garden Suite', 'description', 'tinyurl.com', 'tinyurl.com', 200, 2, 3, 2),
+(3, 3, 'Surrey Penthouse', 'description', 'tinyurl.com', 'tinyurl.com', 800, 2, 5, 3);
+
+INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
+VALUES 
+(1, 2021-09-03, 2021-09-13, 1, 3),
+(2, 2021-09-04, 2021-09-11, 2, 1),
+(3, 2021-09-05, 2021-09-10, 3, 2);
+
+INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
+VALUES 
+(1, 3, 1, 1, 1, 'rate'),
+(2, 1, 2, 2, 1, 'rate'),
+(3, 2, 3, 3, 1, 'rate');
