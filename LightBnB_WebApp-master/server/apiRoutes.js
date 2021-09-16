09dceef8d17fc4af0976bf5cd1a535cc618a080f
Carrie <carr1eg@hotmail.com>
2021-09-16 13:21:21 -0700

    modify users endpoints

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -2,8 +2,11 @@ module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
     database.getAllProperties(req.query, 20)
-    .then(properties => res.send({properties}))
+    .then(properties => {
+      res.send({properties})
+    })
     .catch(e => {
+      console.log('e :>> ', e);
       console.error(e);
       res.send(e)
     }); 
